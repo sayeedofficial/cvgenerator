f9a24941ab04ad88912bee9ffc2315d3c219ae9a
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-01-10 18:58:17 +0530

    Added the functionality to create profiles through forms

--- cvmaker/pdf/views.py
+++ cvmaker/pdf/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render
 from .models import Profile
 
 # Create your views here.
-def home(req):
-    return render(req, "pdf/index.html")
+def home(request):
+    if request.method == "POST":
+        name = request.POST.get("name","");
+        email = request.POST.get("email","");
+        phone = request.POST.get("phone","");
+        summary = request.POST.get("summary","");
+        degree = request.POST.get("degree","");
+        school = request.POST.get("school","");
+        university = request.POST.get("university","");
+        previouswork = request.POST.get("previouswork","");
+        skills = request.POST.get("skills","");
+        
+        profile = Profile(name=name,email=email,phone=phone,summary = summary,degree=degree,school=school,university=university,previouswork=previouswork,skills=skills)
+        profile.save()
+
+
+
+
+
+
+
+
+    return render(request, "pdf/index.html")
 
