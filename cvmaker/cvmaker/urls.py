e81fbadd637d4e9131ccc235a2671493c6dfbada
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-01-10 21:24:03 +0530

    Added Download Functionlity and A basic protype is designed

--- cvmaker/cvmaker/urls.py
+++ cvmaker/cvmaker/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
 from django.urls import path, include
 from pdf import views
@@ -21,4 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="Home"),
     path("<int:id>/", views.resume, name="Resume"),
+    path('list/',views.userslist,name="Users List")
 ]
+
